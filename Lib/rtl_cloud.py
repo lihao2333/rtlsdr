c7a53687be01302945a58ab28f6731d3ab514a34
lihao <lihao3016@gmail.com>
2018-04-08 14:12:44 +0800

    4G

--- Lib/rtl_cloud.py
+++ Lib/rtl_cloud.py
@@ -1,7 +1,11 @@
-
+import time
 from rtlsdr import RtlSdrTcpClient
 def getdata():
-    client = RtlSdrTcpClient(hostname='10.112.216.60', port=12345)
+   # client = RtlSdrTcpClient(hostname='10.112.216.60', port=12345)
+    client = RtlSdrTcpClient(hostname='10.42.0.69', port=2333)
     client.center_freq = 2e6
     data = client.read_samples()
+    client.mytime=time.time()+3
+    print(len(data))
     return data
+getdata()
